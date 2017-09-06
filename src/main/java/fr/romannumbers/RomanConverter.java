f96181b0d3adfc6ad85f96479689ecb41a07a1ef
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-06 12:12:35 +0200

    Convert 2 to II

--- src/main/java/fr/romannumbers/RomanConverter.java
+++ src/main/java/fr/romannumbers/RomanConverter.java
@@ -2,6 +2,8 @@ package fr.romannumbers;
 
 public class RomanConverter {
   public String convertFromArabicToRoman(int toConvert) {
+    if (toConvert == 2)
+      return "II";
     return "I";
   }
 }
--- src/test/java/fr/romannumbers/RomanConverterTest.java
+++ src/test/java/fr/romannumbers/RomanConverterTest.java
@@ -1,13 +1,26 @@
 package fr.romannumbers;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanConverterTest {
+
+  private RomanConverter converter;
+
+  @Before
+  public void setUp() throws Exception {
+    converter = new RomanConverter();
+  }
+
   @Test
   public void should_convert_arabic_1_to_roman_I() throws Exception {
-    RomanConverter converter = new RomanConverter();
     assertThat(converter.convertFromArabicToRoman(1)).isEqualTo("I");
+  }
+
+  @Test
+  public void should_convert_arabic_2_to_roman_II() throws Exception {
+    assertThat(converter.convertFromArabicToRoman(2)).isEqualTo("II");
   }
 }
